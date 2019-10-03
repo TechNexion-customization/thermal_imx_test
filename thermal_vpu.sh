9fb61c859c5c46cac21d24fbc94037e5974eb6d0
Richard Hu <richard.hu@technexion.com>
2019-10-03 01:35:13 -0700

    thermal_vpu.sh: fix warning message from shellcheck

--- thermal_vpu.sh
+++ thermal_vpu.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #################################################################################
 # Copyright 2019 Technexion Ltd.
@@ -19,19 +19,20 @@ H264_FULLHD_LINK='http://samplemedia.linaro.org/H264/big_buck_bunny_1080p_H264_A
 H264_FULLHD_MD5SUM="d68e46b777de4138ed05f3577b03a2c1"
 WESTON_INI=/etc/xdg/weston/weston.ini
 CONNMAN_CONF=/var/lib/connman/test.config
-EXEC_PATH=$(dirname $(readlink -f "$0") )
-GST_LAUNCH='gst-launch-1.0 playbin'
+EXEC_PATH=$(dirname "$(readlink -f "$0")" )
+GST_LAUNCH=$(compgen -c | grep gst-launch)
+GST_LAUNCH_FULL=($GST_LAUNCH playbin)
 BG_PID=0
 TEST_FILE=$EXEC_PATH/test.mp4
 
 #only work under waylands
-function vpu_rotate_display
+vpu_rotate_display()
 {
-    if [ $IS_WAYLAND = 'true' ]; then
+    if [ "$IS_WAYLAND" = 'true' ]; then
         grep '^transform' $WESTON_INI
         if [ "$?" != "0" ]; then
             if [ $((RES_X)) -lt $((RES_Y)) ]; then
-                cp $WESTON_INI $EXEC_PATH && sync
+                cp "$WESTON_INI" "$EXEC_PATH" && sync
                 echo Rotating display 90 degree to landscape mode
 cat << EOF >> $WESTON_INI
 
@@ -54,19 +55,19 @@ EOF
     fi
 }
 
-function connect_network
+connect_network()
 {
     echo Checking network status...
     NETWORK_STATE=$(connmanctl state | grep State | tr -d ' ' | cut -d '=' -f 2)
 
-    if [ $NETWORK_STATE = 'online' ]; then
+    if [ "$NETWORK_STATE" = 'online' ]; then
         echo network is connected!
         return 0
     fi
 
     if [ -s  $CONNMAN_CONF ]; then
-        echo $NETWORK_STATE
-        if [ $NETWORK_STATE = 'idle' ]; then
+        echo "$NETWORK_STATE"
+        if [ "$NETWORK_STATE" = 'idle' ]; then
             echo Removing p2p interface and restarting connman.service...
             ( ifconfig -a | grep -q p2p ) && ( iw dev p2p0 del ) && ( sleep 1 )
             systemctl restart connman.service
@@ -80,7 +81,7 @@ function connect_network
             udhcpc -i eth0
             ;;
         2)
-            ${EXEC_PATH}/configure_wifi.sh
+            "${EXEC_PATH}"/configure_wifi.sh
             ;;
         *)
             echo "Wrong selection!!!"
@@ -94,38 +95,38 @@ function connect_network
     while [ $loop -le 5 ]
     do
          NETWORK_STATE=$(connmanctl state | grep State | tr -d ' ' | cut -d '=' -f 2)
-        if [ $NETWORK_STATE == 'online' ]; then
+        if [ "$NETWORK_STATE" = 'online' ]; then
                 return 0
         else
                 sleep 1
         fi
-        loop=$(expr $loop + 1)
+        loop=$(( loop + 1 ))
     done
 }
 
 
-function vpu_prepare_test_file
+vpu_prepare_test_file()
 {
     
-    if [ ! -s $TEST_FILE ]; then
+    if [ ! -s "$TEST_FILE" ]; then
         echo 'Can not find test.mp4. Start to download test media file...'
         connect_network
-        wget -c $H264_FULLHD_LINK -O $TEST_FILE
-        MD5SUM=$(md5sum $TEST_FILE | awk '{print $(1)}')
-        echo Checking MD5SUM: $MD5SUM
+        wget -c "$H264_FULLHD_LINK" -O "$TEST_FILE"
+        MD5SUM=$(md5sum "$TEST_FILE" | awk '{print $(1)}')
+        echo Checking MD5SUM: "$MD5SUM"
         loop=0
         while [ "$MD5SUM" != "$H264_FULLHD_MD5SUM" ] && [ $loop -le 3 ]
         do
-            wget $H264_FULLHD_LINK -O $TEST_FILE
-            MD5SUM=$(md5sum $TEST_FILE)
-            loop=$(expr $loop + 1)
+            wget "$H264_FULLHD_LINK" -O "$TEST_FILE"
+            MD5SUM=$(md5sum "$TEST_FILE")
+            loop=$( loop + 1 )
         done
     else
         echo 'Find test.mp4!'
     fi
 }
 
-function vpu_play()
+vpu_play()
 {
     #Get real full name of glmark2 on different platform
     vpu_rotate_display && CHANGE_RES=true || CHANGE_RES=false
@@ -135,14 +136,14 @@ function vpu_play()
         echo 3 > /proc/sys/vm/drop_caches
         sleep 1
         if [ $CHANGE_RES = 'true' ]; then
-            echo x: $RES_X
-            echo y: $RES_Y
+            echo x: "$RES_X"
+            echo y: "$RES_Y"
 
             GST_WAYLAND="waylandsink window-width=$RES_Y window-height=$RES_X"
-            echo $GST_WAYLAND
-            $GST_LAUNCH uri=file:$TEST_FILE video-sink=''"$GST_WAYLAND"''
+            echo "$GST_WAYLAND"
+            "${GST_LAUNCH_FULL[@]}" uri=file:"$TEST_FILE" video-sink=''"$GST_WAYLAND"''
         else
-            $GST_LAUNCH uri=file:$TEST_FILE
+            "${GST_LAUNCH_FULL[@]}" uri=file:"$TEST_FILE"
         fi
     done &
 
@@ -151,23 +152,23 @@ function vpu_play()
     sleep 2
     start_time=$(date +%s)
 
-    while [ 1 ]
+    while true
     do
         sleep 1
 
-        t=`cat /sys/class/thermal/thermal_zone0/temp`
-        temperature=`expr $t / 1000`
+        t=$(cat /sys/class/thermal/thermal_zone0/temp)
+        temperature=$(( t/1000 ))
 
         end_time=$(date +%s)
 
-        diff_time=$(($end_time-$start_time))
+        diff_time=$(( end_time-start_time ))
 
         echo
 
         echo "===============================" | tee -a $LOG
         printf "Running VPU playback test \n"
         echo -n "Elapsed time: " && date -d@$diff_time -u +%H:%M:%S
-        printf "Temperature: %s degree \n" $temperature | tee -a $LOG
+        printf "Temperature: %s degree \n" "$temperature" | tee -a $LOG
         echo "===============================" | tee -a $LOG
         sync
 
@@ -181,11 +182,11 @@ function trap_ctrlc ()
     echo "Ctrl-C caught...performing clean up"
 
     echo "killall $GST_LAUNCH"
-    killall $GST_LAUNCH
-    kill $BG_PID
+    killall "$GST_LAUNCH"
+    kill "$BG_PID"
     # exit shell script with error code 2
     # if omitted, shell script will continue execution
-    cp $EXEC_PATH/weston.ini $WESTON_INI && sync
+    cp "$EXEC_PATH"/weston.ini "$WESTON_INI" && sync
     systemctl restart weston
     echo
     exit 2
@@ -197,21 +198,21 @@ if [ -f $WESTON_INI ]; then
     IS_WAYLAND=true
     echo Running wayland...
     CONNECTOR=$( modetest -c | grep connected | awk '{print $(NF-3)}' )
-    echo $CONNECTOR
+    echo "$CONNECTOR"
     RES=$( modetest -p | grep -A3 CRTCs | awk 'END {print $(1)}' )
-    if [ $RES == '0' ]; then
+    if [ "$RES" = '0' ]; then
         echo There is no valid display
         return 0
     else
-        echo $RES
-        RES_X=$(echo $RES | cut -d 'x' -f 1)
-        RES_Y=$(echo $RES | cut -d 'x' -f 2)
+        echo "$RES"
+        RES_X=$(echo "$RES" | cut -d 'x' -f 1)
+        RES_Y=$(echo "$RES" | cut -d 'x' -f 2)
         echo Resolution:
-        echo x: $RES_X
-        echo y: $RES_Y
+        echo x: "$RES_X"
+        echo y: "$RES_Y"
     fi
 else
-    ISWAYLAND=false
+    IS_WAYLAND=false
 fi
 
 vpu_prepare_test_file
