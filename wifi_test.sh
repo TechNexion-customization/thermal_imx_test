bb17d9bcf05a0b3d4c3213af310bfa21cf74a2a6
Ray Chang <ray.chang@technexion.com>
2020-11-20 16:24:45 +0800

    wifi_test.sh: fix the reconnection function and debugging optimization

--- wifi_test.sh
+++ wifi_test.sh
@@ -15,6 +15,7 @@ IPERF_SERVER=10.88.88.88
 IPERF_PORT=5201
 WIFI_DRV=wlan
 IFACE=wlan0
+NETWORK_MANAGER=connmanctl
 
 is_ap_connected() {
         for n in {1..30}; do
@@ -52,8 +53,17 @@ led_off() {
         fi
 }
 
+led_blink() {
+        while true; do
+                led_on
+                usleep 150000
+                led_off
+                usleep 150000
+        done
+}
+
 iperf_test() {
-        iperf3 -f k -c "$IPERF_SERVER" -p "$IPERF_PORT" -t 7 -w 320k -P 4 -l 24000 >/tmp/iperflog
+        iperf3 -f k -c "$IPERF_SERVER" -p "$IPERF_PORT" -t 8 -w 320k -P 4 -l 24000 >/tmp/iperflog
 }
 
 if [ "$CPUBURN" == "yes" ]; then
@@ -73,41 +83,46 @@ if [ "$CPUBURN" == "yes" ]; then
         stress-ng -c 0 -l 80 &
 fi
 
-if (is_ap_connected); then
-        if (is_ipget); then
+if ( is_ap_connected ); then
+        if ( is_ipget ); then
                 usleep 100000
-                count=0
                 samecount=0
                 ret=-1
                 oldret=-1
                 while true; do
-                        [ $samecount -lt 5 ] && [ -n "$ret" ] && [ $ret -ne 0 ] && led_on
-                        [ $count -eq 1 ] && iperf_test &
-                        if [ $count -gt 43 ]; then
-                                ret=$(tail -n3 </tmp/iperflog | head -n1 | awk '{printf("%d", $6)}')
-                                if [ "$ret" == "$oldret" ]; then samecount=$((samecount + 1)); else samecount=0; fi
-                                oldret=$ret
-                                [ $samecount -gt 5 ] && ret=0
-                                [ $samecount -ge 3 ] && samecount=0 && killall iperf3 && sleep 2 && is_ap_connected && is_ipget && iperf_test &
-                                cputmp=$(sed 's/000//' /sys/class/thermal/thermal_zone0/temp)
-                                cpufreq=$(sed 's/000//' /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq)
-                                timestamp=$(date +"[%D %T]")
-                                if [ "$WIFI_DRV" != "wlan" ] && [ "$WIFI_DRV" != "qca9377" ]; then
-                                        echo "$timestamp CPU Freq/Temp: $cpufreq MHz/$cputmp ^C, BW: $ret KBits/sec" | tee -a /tmp/wifilog_"$mac"
-                                else
-                                        chiptmp=$(iwpriv wlan0 get_temp | cut -d : -f2 | tr -d ' ')
-                                        wifirssi=$(iwpriv wlan0 getRSSI | cut -d = -f2)
-                                        echo "$timestamp CPU Freq/Temp: $cpufreq MHz/$cputmp ^C, WiFi RSSI: $wifirssi, Temp: $chiptmp, BW: $ret KBits/sec" | tee -a /tmp/wifilog_"$mac"
-                                fi
-                                count=0
+                        if [ $samecount -eq 0 ] && [ -z "$led_blink_pid" ]; then
+                                led_blink >/dev/null 2>&1 &
+                                led_blink_pid=$!
                         fi
-                        usleep 100000
-                        led_off
-                        usleep 100000
-                        count=$((count + 1))
+                        if ( is_ap_connected >/dev/null 2>&1 ); then
+                                ( is_ipget >/dev/null 2>&1 ) && iperf_test &
+                        else
+                                if [ "$NETWORK_MANAGER" == connmanctl ]; then
+                                        ap=$($NETWORK_MANAGER services | grep "*A " | cut -d' ' -f4)
+                                        [ -n "$ap" ] && $NETWORK_MANAGER connect $ap
+                                fi
+                        fi
+                        usleep 9950000
+                        ret=$(tail -n3 </tmp/iperflog | head -n1 | awk '{printf("%d", $6)}')
+                        if [ "$ret" == "$oldret" ] || [ -z "$ret" ] || [ $ret -eq 0 ]; then samecount=$((samecount + 1)); else samecount=0; fi
+                        oldret=$ret
+                        if [ $samecount -ge 3 ]; then
+                                killall iperf3
+                                [ -n "$led_blink_pid" ] && kill $led_blink_pid && led_blink_pid=""
+                                ret=0
+                        fi
+                        cputmp=$(sed 's/000//' /sys/class/thermal/thermal_zone0/temp)
+                        cpufreq=$(sed 's/000//' /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq)
+                        timestamp=$(date +"[%D %T]")
+                        if [ "$WIFI_DRV" != "wlan" ] && [ "$WIFI_DRV" != "qca9377" ]; then
+                                echo "$timestamp CPU Freq/Temp: $cpufreq MHz/$cputmp ^C, BW: $ret KBits/sec" | tee -a /tmp/wifilog_"$mac"
+                        else
+                                chiptmp=$(iwpriv wlan0 get_temp | cut -d : -f2 | tr -d ' ')
+                                wifirssi=$(iwpriv wlan0 getRSSI | cut -d = -f2)
+                                echo "$timestamp CPU Freq/Temp: $cpufreq MHz/$cputmp ^C, WiFi RSSI: $wifirssi, Temp: $chiptmp, BW: $ret KBits/sec" | tee -a /tmp/wifilog_"$mac"
+                        fi
                 done
         fi
 fi
 
 led_off
-
